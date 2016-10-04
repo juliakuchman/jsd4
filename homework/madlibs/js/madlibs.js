f040dedcec15725bc23ff16738cdf759d337de9c
juliakuchman <juliakuchman@gmail.com>
2016-10-03 18:09:16 -0700

    hw

--- homework/madlibs/js/madlibs.js
+++ homework/madlibs/js/madlibs.js
@@ -33,6 +33,11 @@ generateStartup();
 
 // the generateStartup function is called when
 // the "Generate New Startup" button is clicked
+
+// The Math.floor() function 
+// returns the largest integer less than or equal to a given number.
+
+
 function generateStartup() {
 
 	// TODO: generate two random index numbers, one for each array
@@ -46,21 +51,18 @@ function generateStartup() {
 	//       to create a new startup idea like:
 	//       "A startup that is Apple, but for Trello"
 	startupIdea = "A startup that is " + startupX[random1] + ", but for "+ startupY[random2];
-	console.log(startupIdea);
 
 	// DONE: Update page with new startup idea
 	startup.innerHTML = startupIdea;
 }
 
 
-
 // the saveFavorite function is called when
 // the "Save Favorite" button is clicked
 function saveFavorite() {
-
 	// TODO: add the new idea to the array
-	var newIdea = startupIdea;
-
+	// I may need to find how to only save the one displayed
+	favorites.push(startupIdea);
 }
 
 
@@ -71,12 +73,32 @@ function printFavorites() {
 	// DONE: clear out favorites section each time
 	// before displaying new list of favorites
 	list.innerHTML = '';
-
-
 	// TODO: concatenate all the favorites into one string
 	// - hint: loop through all the favorites
 	// - this should be stored in a variable named favoritesText
 	// - each favorite should have an html br element between it (EG: "<br>")
+	
+	// I think i need to take some of this out 
+	// var favoritesString = favorites.toString();
+	// var splitString = favoritesString.split();
+	// console.log("splitString", splitString);
+	
+	// favoritesText
+	var favoritesText = "";
+		for (var i = 0; i < favorites.length; i++) {
+			if (i + 1 < favorites.length) {
+				favoritesText = favoritesText + favorites[i] + ' <br>'
+			}
+				else {
+					favoritesText = favoritesText + favorites[i]
+				}
+
+			// var favoritesString = favorites.toString();
+			
+			// favoritesText.push(favorites.toString());
+			// console.log("favorites text", favoritesText);
+		};
+
 
 
 
