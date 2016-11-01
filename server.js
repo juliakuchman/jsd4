b7d4d484fb1f25e98b83bb564b18707fade6173a
juliakuchman <juliakuchman@gmail.com>
2016-10-31 21:40:17 -0700

    idk

--- server.js
+++ server.js
@@ -2,12 +2,11 @@ var express = require('express');
 var app = express();
 var router = express.Router();
 
-//app.set('port', 5000);
+app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
-app.use(router);
+app.use("/", router);
 
-
-app.listen(5000, function(){
-	console.log("Node app is running on port", 5000)
+app.listen(app.get("port"), function() {
+	app.get("port");
 });
